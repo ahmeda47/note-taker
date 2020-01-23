aba45af9ca1c236e2a12b1cbddb07ed2fef646ae
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-01-23 02:51:17 -0500

    added filter to delete method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,17 +9,9 @@ module.exports = function(app){
         res.send('put data in api');
     });
     app.delete('/api/notes', (req, res) => {
-        console.log(req.body);
-        console.log(noteData);
-        let temp = noteData.map(x => {
-            if (x.title === req.body.title && x.description === req.body.description){
-                return false;
-                console.log(x.title);
-            } else {
-                return x;
-            }
-        });
-
+        //console.log(req.body);
+        //console.log(noteData);
+        let temp = noteData.filter(x=>!x.title.includes(req.body.title) && !x.description.includes(req.body.description));
         noteData = temp;
         res.send(noteData);
     });
